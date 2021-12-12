4c6e63fdb2f159cb70d9fa7c40cb046af8e00023
sagarstha254 <sagarstha254@gmail.com>
2021-12-12 20:49:06 +0545

    takes two number and returs sum of them

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 console.log("hello world")
-let name = prompt("Enter your name");
-
-if (name === "suman"){
-    alert("You are my god father");
-} else if ( name === "sagar"){
-    alert("You are my friend");
-}
-else {
-    alert('vak sale chor');
-}
+ var a = prompt("enter first number");
+ var b = prompt("enter second number");
+var sum = parseInt(a) + parseInt(b);
+alert(sum)
