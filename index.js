40d491507d96d56880520b5187cb9aa5b00645b0
sagarstha254 <sagarstha254@gmail.com>
2021-12-12 20:44:53 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("hello world")
+let name = prompt("Enter your name");
+
+if (name === "suman"){
+    alert("You are my god father");
+} else if ( name === "sagar"){
+    alert("You are my friend");
+}
+else {
+    alert('vak sale chor');
+}
